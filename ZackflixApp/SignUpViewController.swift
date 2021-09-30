8fd8b0b0afbf3f43789b249c01e311d8f6a2f0c6
EthanPark <ethanpark604@gmail.com>
2021-10-01 00:10:02 +0900

    Switch 액션 추가

--- ZackflixApp/SignUpViewController.swift
+++ ZackflixApp/SignUpViewController.swift
@@ -14,26 +14,60 @@ class SignUpViewController: UIViewController {
     @IBOutlet var nicknameText: UITextField!
     @IBOutlet var whereText: UITextField!
     @IBOutlet var recommendText: UITextField!
-    
     @IBOutlet var emailText: UITextField!
+    @IBOutlet var jackflixSwitch: UISwitch!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         emailText.attributedPlaceholder = NSAttributedString(string: "이메일 주소 또는 전화번호", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        emailText.keyboardType = .emailAddress
+        
         passwordText.attributedPlaceholder = NSAttributedString(string: "비밀번호", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        passwordText.keyboardType = .numberPad
+        
         nicknameText.attributedPlaceholder = NSAttributedString(string: "닉네임", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        
+        
         whereText.attributedPlaceholder = NSAttributedString(string: "위치", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        
+        
         recommendText.attributedPlaceholder = NSAttributedString(string: "추천 코드 입력", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
         
         
     
         signUpButton.layer.cornerRadius = 7
+        signUpButton.setTitle("회원가입", for: .normal)
+        signUpButton.setTitleColor(UIColor.black, for: .normal)
+        signUpButton.backgroundColor = .white
         
+        jackflixSwitch.setOn(true, animated: true)
+        jackflixSwitch.onTintColor = .systemRed
+        jackflixSwitch.thumbTintColor = .white
         
-        
-      
     }
     
-
+    @IBAction func downGesture(_ sender: UITapGestureRecognizer) {
+        
+        view.endEditing(true)
+        
+    }
+    @IBAction func clickedSwitch(_ sender: UISwitch) {
+        if jackflixSwitch.isOn {
+            nicknameText.isHidden = false
+            whereText.isHidden = false
+            recommendText.isHidden = false
+        } else {
+            nicknameText.isHidden = true
+            whereText.isHidden = true
+            recommendText.isHidden = true
+        }
+        
+        
+    }
+    
+    
+    
 }
